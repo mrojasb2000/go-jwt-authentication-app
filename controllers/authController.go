a54c780965cf397a78a4cc5c644141fcd5631179
mavro <mrojasb2000@gmail.com>
2023-09-17 18:26:48 -0300

    chore: add register route

--- controllers/authController.go
+++ controllers/authController.go
@@ -5,3 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello World!")
 }
+
+func Register(c *fiber.Ctx) error {
+	return c.SendString("Register")
+}
