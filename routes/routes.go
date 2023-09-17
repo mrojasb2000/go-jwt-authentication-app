4ffa19d57159564d36e920366e835fcb0314b0d3
mavro <mrojasb2000@gmail.com>
2023-09-17 18:11:50 -0300

    chore: add auth handler controller

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,10 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mrojasb2000/go-jwt-authentication-app/controllers"
+)
 
 func Setup(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!")
-	})
+	app.Get("/", controllers.Hello)
 }
