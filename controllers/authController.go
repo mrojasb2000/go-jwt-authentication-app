ab36977ec5cc50cd811af58edd08bd29b738ded2
mavro <mrojasb2000@gmail.com>
2023-09-17 18:31:28 -0300

    chore: implement POST register data

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,11 +1,21 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello World!")
 }
 
 func Register(c *fiber.Ctx) error {
-	return c.SendString("Register")
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return c.JSON(fmt.Sprintf("Error: %s", err))
+	}
+
+	return c.JSON(data)
 }
