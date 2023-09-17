5443e11dbbf93e7a069f88db36886386101fb112
mavro <mrojasb2000@gmail.com>
2023-09-17 17:21:27 -0300

    chore: add reading environment variables

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(mysql.Open("username:password@/dbname"), &gorm.Config{})
+	username := goDotEnvVariable("USERNAME")
+	password := goDotEnvVariable("PASSWORD")
+	dbname := goDotEnvVariable("DBNAME")
+
+	connectionString := fmt.Sprintf("%s:%s@/%s", username, password, dbname)
+	_, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to database.")
 	}
@@ -19,4 +29,18 @@ func main() {
 	})
 
 	app.Listen(":3000")
+}
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
